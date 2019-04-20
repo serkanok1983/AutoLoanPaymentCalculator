79df9a7667771497e5246351d66d6bf2c5d3a468
Serkan Ok <serkanok1983@yahoo.com>
2019-04-20 23:58:53 +0300

    Initial commit

---
+++ app/src/main/java/com/serkanok/autoloanpaymentcalculator/AnaEkran.java
@@ -0,0 +1,100 @@
+package com.serkanok.autoloanpaymentcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AnaEkran extends AppCompatActivity {
+
+    int krediTutari;
+    double faizOrani;
+    int vade;
+    TextView aylikOdemeOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ana_ekran);
+
+        krediTutari = 0;
+        faizOrani = 0;
+        vade = 0;
+        EditText krediTutariGirdi = findViewById(R.id.kredi_tutari_input);
+        EditText faizOraniGirdi = findViewById(R.id.faiz_orani_input);
+        EditText vadeGirdi = findViewById(R.id.vade_input);
+        aylikOdemeOutput = findViewById(R.id.aylik_odeme_output);
+        krediTutariGirdi.addTextChangedListener(krediTutariGirdiIzleyici);
+        faizOraniGirdi.addTextChangedListener(faizOraniGirdiIzleyici);
+        vadeGirdi.addTextChangedListener(vadeGirdiIzleyici);
+    }
+
+    private void sonucGoster() {
+        double a = Math.pow((1 + faizOrani), vade);
+        aylikOdemeOutput.setText(String.format("%.1f", (krediTutari * (faizOrani * a) / (a - 1))));
+    }
+
+    private TextWatcher krediTutariGirdiIzleyici = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.toString().length() > 0) {
+                krediTutari = Integer.parseInt(s.toString());
+                sonucGoster();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private TextWatcher faizOraniGirdiIzleyici = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.toString().length() > 0) {
+                faizOrani = Double.parseDouble(s.toString());
+                sonucGoster();
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private TextWatcher vadeGirdiIzleyici = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.toString().length() > 0) {
+                vade = Integer.parseInt(s.toString());
+                sonucGoster();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+}
